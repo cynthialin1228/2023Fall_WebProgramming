02174cafcd3f52f2022d6b790bbb20aaaa32226e
cynthialin1228 <cynthialin1228@gmail.com>
2023-10-08 19:07:06 +0800

    Playlist.tsx

--- hw2/frontend/src/App.tsx
+++ hw2/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import { Add as AddIcon } from "@mui/icons-material";
 import { Button } from "@mui/material";
-
+import Playlists from "@/components/Playlists";
 import CardList from "@/components/CardList";
 import HeaderBar from "@/components/HeaderBar";
 import NewListDialog from "@/components/NewListDialog";
@@ -45,7 +45,7 @@ function App() {
 
       <main className="mx-auto grid max-h-full md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-24 py-12">
         {lists.map((list) => (
-          <CardList key={list.id} {...list} showDeleteIcon={showDeleteIcon} />
+          <Playlists key={list.id} {...list} showDeleteIcon={showDeleteIcon} />
         ))}
         
         <NewListDialog
