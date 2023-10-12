c58306457daf7af8e4545bf8aebeca89184a46bb
cynthialin1228 <cynthialin1228@gmail.com>
2023-10-12 11:55:29 +0800

    1.1

--- hack1/frontend/src/routes/auth/Layout.tsx
+++ hack1/frontend/src/routes/auth/Layout.tsx
@@ -79,8 +79,8 @@ const AuthLayout = () => {
                 className="last-of-type:border-r-0"
                 data-testid={`tab-${tab.path}`}
               >
-                <NavLink to= {tab.path}/>
-                {/* <NavLink to={tab.path}>{tab.title}</NavLink> */}
+                {/* <NavLink to= {tab.path}/> */}
+                <NavLink to={tab.path}>{tab.title}</NavLink>
               </TabsTrigger>
               /* End of TODO 1.3 */
             ))}
